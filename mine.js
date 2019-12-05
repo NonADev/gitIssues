2a9eeb9b21b6a669b0d0da7cbc89035ce3b0bcd7
nonadev <wesley.vieira.lima.1999@gmail.com>
2019-12-05 14:40:07 -0300

    funcionando

--- mine.js
+++ mine.js
@@ -1,4 +1,5 @@
 "use strict";
+var fs = require("fs");
 const user = "apache";
 const repository = "cordova-android";
 const fetch = require("node-fetch"); 
@@ -11,13 +12,19 @@ async function getIssues(a){
     return resp.json();
 }
 (function(){
+    fs.writeFile(`./${user}_${repository}.csv`, "", (err) => {
+        if(err) {
+            console.log(err);
+        }
+    });
     (async function(){ 
-        var link = `https://api.github.com/search/issues?q=repo:${user}/${repository}+type:issue&per_page=1`;
+        var link = `https://api.github.com/search/issues?q=repo:${user}/${repository}+type:issue+state:open&per_page=1`;
         var resp = await fetch(link);
         return resp.json();
     })().then(function(r){
         //console.log(Math.ceil(r.total_count/30));
         pages = Math.ceil(r.total_count/30);
+        console.log(r.total_count);
         for(i=1;i<=pages;i++){
             getIssues(i).then(function(r){
                 if(r.length==0) {
@@ -25,7 +32,17 @@ async function getIssues(a){
                 }
                 for(var i=0;i<r.length;i++){
                     var element = r[i];
-                    console.log(`"${element.title.replace(/(\r\n|\n|\r)/gm,"")}","${element.body.replace(/(\r\n|\n|\r)/gm,"")}","${element.state.replace(/(\r\n|\n|\r)/gm,"")}","${element.number}","${element.assign}","${element.labels}","${element.milestone}"`);
+                    var data = `"${element.title.replace(/(\r\n|\n|\r)/gm,"")}","${element.body.replace(/(\r\n|\n|\r)/gm,"")}","${element.state.replace(/(\r\n|\n|\r)/gm,"")}","${element.number}","${element.assign}","${element.labels}","${element.milestone}"\n`;
+                    
+                    fs.appendFile(`./${user}_${repository}.csv`, data, (err) => {
+                        if(err) {
+                            console.log(err);
+                        }
+                        else{
+                            //console.log("Successfully Written to File. :"+data);
+                        }
+                    });
+                    //console.log(`"${element.title.replace(/(\r\n|\n|\r)/gm,"")}","${element.body.replace(/(\r\n|\n|\r)/gm,"")}","${element.state.replace(/(\r\n|\n|\r)/gm,"")}","${element.number}","${element.assign}","${element.labels}","${element.milestone}"`);
                 }
             });
         }
