65e8643a3c86c8635c33afb798023bed0e03b7ec
nonadev <wesley.vieira.lima.1999@gmail.com>
2019-12-06 14:48:03 -0300

    alguns tratamentos de 3º argumento

--- mine.js
+++ mine.js
@@ -2,10 +2,21 @@
 
 var fs = require("fs");
 const fetch = require("node-fetch"); 
-let pages = 0;
-let i = 0;
-let userRep = process.argv[2].substring(19,process.argv[2].length);
-let restUrl = `https://api.github.com/repos/${userRep}/issues?page=`;
+if(process.argv.length<3){ //tratamento de parametro : tem que ter paremtro 3
+    console.log('Aplication usage example: \nnode mine.js https://github.com/apache/cordova-android');
+    return;
+}
+if(process.argv[2].charAt(process.argv[2].length-1)=='/'){ //tratamento de last char : tem que terminar diferente de '/'
+    console.log('Be aware of the last character, have to be a letter or number');
+    process.argv[2] = process.argv[2].substring(0,process.argv[2].length-1);
+}
+let userRep = process.argv[2].substring(19,process.argv[2].length); //getUrlBy parameter
+let restUrl = `https://api.github.com/repos/${userRep}/issues?page=`; //use url parameter to get the api rest link
+
+if(process.argv[2].substr(0,19)!="https://github.com/"){
+    console.log('The link sended has to be using the "https" protocol. \nExample: https://github.com/apache/cordova-android');
+    return;
+}
 async function getIssues(a){
     var resp = await fetch(restUrl+a);
     //console.log(restUrl+a);
@@ -21,11 +32,11 @@ let fileToWrite = `./${userRep.replace("/","_")}.csv`;
     (async function(){ 
         getIssues(1).then(function(r){ //getIssues(i).then(function(r){
             if(r.message=='Not Found'){
-                console.log(`The repository ${restUrl} do not exists`);
+                console.log(`The repository ${process.argv[2]} do not exists`);
                 return;
             }
             if(r.length==0) {
-                console.log(`The repository ${restUrl} has no issues`);
+                console.log(`The repository ${process.argv[2]} has no issues`);
                 return; //working
             }
             for(var i=0;i<r.length;i++){
@@ -38,7 +49,7 @@ let fileToWrite = `./${userRep.replace("/","_")}.csv`;
                 if(element.milestone!=null && 'title' in element.milestone){
                     milestone = element.milestone.milestone; //if true
                 }
-                var data = `"${element.title.replace(/(\r\n|\n|\r)/gm,"")}","${element.body.replace(/(\r\n|\n|\r)/gm,"")}","${element.state.replace(/(\r\n|\n|\r)/gm,"")}","${element.number}","${assignee}","${element.labels}","${milestone}"\n`;
+                var data = `"${element.title.replace(/(\r\n|\n|\r)/gm,"")}","${element.body.replace(/(\r\n|\n|\r)/gm,"").replace('"', "'")}","${element.state.replace(/(\r\n|\n|\r)/gm,"")}","${element.number}","${assignee}","${element.labels}","${milestone}"\n`;
                 fs.appendFile(fileToWrite, data, (err) => {
                     if(err) {
                         console.log(err);
