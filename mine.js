34c369caf749773866fa171f6dd5b660c039228c
nonadev <wesley.vieira.lima.1999@gmail.com>
2019-12-09 15:06:56 -0300

    options for input errors added

--- mine.js
+++ mine.js
@@ -6,17 +6,22 @@ if(process.argv.length<3){ //tratamento de parametro : tem que ter parametros 3
     return;
 }
 if(process.argv[2].charAt(process.argv[2].length-1)=='/'){ //tratamento de last char : tem que terminar diferente de '/'
-    console.log('Be aware of the last character, have to be a letter or number');
+    console.log('Be aware of the last character, have to be a letter or a number');
     process.argv[2] = process.argv[2].substring(0,process.argv[2].length-1);
 }
 let userRep = process.argv[2].substring(19,process.argv[2].length); //getUrlBy parameter
-let restUrl = `https://api.github.com/repos/${userRep}/issues?page=`; //use url parameter to get the api rest link
+let restUrl = `https://api.github.com/repos/${userRep}/issues?page=1&per_page=100`; //use url parameter to get the api rest link
 if(process.argv[2].substr(0,19)!="https://github.com/"){ //tratamento de 3 parametros que tem que ter o https ao invez de n ter ou ser http
-    console.log('The link sended has to be using the "https" protocol. \nExample: https://github.com/apache/cordova-android');
+    if(process.argv[2].substr(0,5)!="https"){
+        console.log('The link sent has to be using the "https" protocol. \nExample: https://github.com/apache/cordova-android');
+    }
+    else if(process.argv[2].substr(8,11)!="github.com/"){
+        console.log('The site is wrong (correct = github.com), or is in other Encoding different than UTF-8');
+    }
     return;
 }
-async function getIssues(a){
-    var resp = await fetch(restUrl+a+"&per_page=100");
+async function getIssues(){
+    var resp = await fetch(restUrl);
     //console.log(restUrl+a);
     return resp.json();
 }
@@ -28,7 +33,7 @@ let fileToWrite = `./${userRep.replace("/","_")}.csv`;
         }
     });
     (async function(){ 
-        getIssues(1).then(function(r){ //getIssues(i).then(function(r){
+        getIssues().then(function(r){ //getIssues(i).then(function(r){
             if(r.message=='Not Found'){
                 console.log(`The repository ${process.argv[2]} do not exists`);
                 return;
